0e24b5454ce1cbf4d16b0b1590d21536f532c566
sdac-22 <sdac-22@sdac-22-PC>
2024-11-25 11:49:11 +0530

    policy

---
+++ git_pbyc/pbyc/src/com/contract/Policy.java
@@ -0,0 +1,8 @@
+package com.contract;
+
+public interface Policy {
+	double calsal (double sal);
+	double calsal (double sal, double bonus);
+	double calsal (double sal, double bonus, double tax);
+	
+}
